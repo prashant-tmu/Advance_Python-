de6dfb6b8d5469e8c5a6d643532aeb2683c98d23
prashant kumar <prashantkumar781803@gmail.com>
2025-07-31 16:27:34 +0530

    Update insta.py

--- insta.py
+++ insta.py
@@ -1,23 +1,22 @@
 from instagrapi import Client
 import requests
 
-# Create a client instance
+
 cl = Client()
 
-# 🔐 Login to Instagram
-cl.login("wihol866425689456", "6joqf(3ix8f3")  # Replace with your username & password
 
-# 👤 Target username
+cl.login("wihol866425689456", "6joqxxxxxxxxxx")  
+
+
 target_username = "username target"
 
-# 📥 Get user info
+
 user_info = cl.user_info_by_username(target_username)
 
-# 🖼️ Get profile picture URL
+
 profile_pic_url = user_info.profile_pic_url
 print("Profile Picture URL:", profile_pic_url)
 
-# 💾 Download and save the image
 response = requests.get(profile_pic_url)
 
 if response.status_code == 200:
